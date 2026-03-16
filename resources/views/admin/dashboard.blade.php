0867009f429fe295b1712ce3d8e0fa6384edf9c1
Levis Hien <hienlevis0@gmail.com>
2026-03-16 22:19:04 +0000

    Initial commit

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <h1 class="text-2xl font-bold mb-4">Bienvenue sur le tableau de bord administrateur</h1>
+    <p>Seuls les administrateurs peuvent voir cette page.</p>
+</div>
+@endsection
